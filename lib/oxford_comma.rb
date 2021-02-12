22e7a59738e3afe91e005bd6fa968b81c20a7423
Naomi-Rhames <naomirhames2020@gmail.com>
2021-02-12 08:37:36 -0600

    commit tree c2eb24c3cf7d1af1cf33b6eaf4f178647246cf39

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
    if array.length <= 2
-    array.join( " and ")
+      array.join( " and ")
    else array.length >= 3
     array[-1] = "and " + array[-1] 
     array.join(", ")
